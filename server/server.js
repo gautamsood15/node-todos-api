4321fdaf3b64a11d21867e8c822e4fc1f7d31bda
gautamsood15 <guaty22@gmail.com>
2018-09-25 23:31:11 +0530

    validators and types and defaults added

--- server/server.js
+++ server/server.js
@@ -5,17 +5,17 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 //Model -- defines the template of the documents in a collection so that all documents have a unified format
 
-var Todo = mongoose.model('Todo', {          // model created
-  text : {
-    type : String
-  }, completed : {
-    type : Boolean
-  }, completedAt : {
-    type :  Number
-  }
-});
+// var Todo = mongoose.model('Todo', {          // model created
+//   text : {
+//     type : String
+//   }, completed : {
+//     type : Boolean
+//   }, completedAt : {
+//     type :  Number
+//   }
+// });
 //
-// var newTodo = new Todo({                      // creating a document
+// var newTodo = new Todo({                      // creating a document    // mongoose does have TYPE CASTING
 //   text : 'Cook dinner'
 // });
 //
@@ -25,18 +25,57 @@ var Todo = mongoose.model('Todo', {          // model created
 //   console.log('Unable to save todo');
 // });
 
+// var Todo = mongoose.model('Todo', {
+//   text : {
+//     type : String,
+//     required : true,
+//     minlength : 1,
+//     trim : true
+//   }, completed : {
+//     type : Boolean,
+//     default : false
+//   }, completedAt : {
+//     type :  Number,
+//     default : null
+//   }
+// });
+//
+// var otherTodo = new Todo({
+//   text : true
+//
+// });
+//
+//
+// otherTodo.save().then((doc) => {
+//   console.log('Saved todo');
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save the todo', e);
+// });
 
 
-var otherTodo = new Todo({
-  text : 'Feed the cat',
-  completed : true,
-  completedAt : 123
+//User
+//email - require it - trim it - set type - minlength : 1
+
+
+
+
+var User = mongoose.model('User', {
+    email : {
+      required : true,
+      trim : true,
+      type : String,
+      minlength : 1
+  }
 });
 
 
-otherTodo.save().then((doc) => {
-  console.log('Saved todo');
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save the todo');
+var newUser = new User({
+  email :  'andrew@example.com        '
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved User', doc);
+}, (e) =>  {
+  console.log('Unable to save user', e);
 });
